248099817471513ed88c3d90c0348c2bda45e75e
brandonanzalone <banzalone3@students.mchenry.edu>
2017-06-13 18:03:19 -0500

    Add files via upload

---
+++ Ifelse.py
@@ -0,0 +1,36 @@
+number = int(input('Enter a number between 1 and 10. '))
+
+if number == 1:
+    print("|")
+else:
+    if number == 2:
+        print("||")
+    else:
+        if number == 3:
+            print("|||")
+        else:
+            if number == 4:
+                print("|V")
+            else:
+                if number == 5:
+                    print("V")
+                else:
+                    if number == 6:
+                        print("V|")
+                    else:
+                        if number == 7:
+                            print("V||")
+                        else:
+                            if number == 8:
+                                print("V|||")
+                            else:
+                                if number == 9:
+                                    print("|X")
+                                else:
+                                    if number == 10:
+                                        print("X")
+                                    else:
+                                        print("Please choose a number between 1 and 10.")
+
+
+
